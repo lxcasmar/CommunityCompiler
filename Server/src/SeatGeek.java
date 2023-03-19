ee8ec91151092d9e2590a3043fd3bcc37006aa67
lxcasmar <ljcasmar0o0@gmail.com>
2023-03-19 15:57:56 -0400

    add: okhttp library jar

---
+++ Server/src/SeatGeek.java
@@ -0,0 +1,10 @@
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.Response;
+
+public class SeatGeek {
+    private static final String BASE_URL = "https://api.seatgeek.com/2/";
+    private final OkHttpClient client = new OkHttpClient();
+
+    
+}
