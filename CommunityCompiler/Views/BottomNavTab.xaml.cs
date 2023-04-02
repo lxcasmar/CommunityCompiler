b6234e2480a01788c5400bea608c0f11189b38b3
vyn203 <vyn1@pitt.edu>
2023-04-02 19:03:23 -0400

    Add Selected/Unselected Bottom Nav Icons

--- CommunityCompiler/Views/BottomNavTab.xaml.cs
+++ CommunityCompiler/Views/BottomNavTab.xaml.cs
@@ -1,9 +1,57 @@
-﻿namespace CommunityCompiler.Views;
+﻿using Microsoft.Maui.Controls;
+using Microsoft.Maui.Controls.Platform;
+
+namespace CommunityCompiler.Views;
 
 public partial class BottomNavTab : TabbedPage
 {
-	public BottomNavTab()
+    public BottomNavTab()
 	{
-		InitializeComponent();
-	}
+        InitializeComponent();
+
+        nav_home.IconImageSource = "selected_home.png";
+        nav_search.IconImageSource = "unselected_search.png";
+        nav_settings.IconImageSource = "unselected_settings.png";
+        nav_favorites.IconImageSource = "unselected_favorites.png";
+
+        CurrentPageChanged += CurrentPageHasChanged;
+    }
+
+    private void CurrentPageHasChanged(object sender, EventArgs e)
+    {
+        var tabbedPage = (TabbedPage)sender;
+        Title = tabbedPage.CurrentPage.Title;
+
+        if (Title == "Home")
+        {
+            nav_home.IconImageSource = "selected_home.png";
+            nav_search.IconImageSource = "unselected_search.png";
+            nav_settings.IconImageSource = "unselected_settings.png";
+            nav_favorites.IconImageSource = "unselected_favorites.png";
+        }
+
+        if (Title == "Search")
+        {
+            nav_home.IconImageSource = "unselected_home.png";
+            nav_search.IconImageSource = "selected_search.png";
+            nav_settings.IconImageSource = "unselected_settings.png";
+            nav_favorites.IconImageSource = "unselected_favorites.png";
+        }
+
+        if (Title == "Settings")
+        {
+            nav_home.IconImageSource = "unselected_home.png";
+            nav_search.IconImageSource = "unselected_search.png";
+            nav_settings.IconImageSource = "selected_settings.png";
+            nav_favorites.IconImageSource = "unselected_favorites.png";
+        }
+
+        if (Title == "Favorites")
+        {
+            nav_home.IconImageSource = "unselected_home.png";
+            nav_search.IconImageSource = "unselected_search.png";
+            nav_settings.IconImageSource = "unselected_settings.png";
+            nav_favorites.IconImageSource = "selected_favorites.png";
+        }
+    }
 }
