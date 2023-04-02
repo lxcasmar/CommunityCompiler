263e4324671a09e2781f65d7a1fd863128a90606
vyn203 <vyn1@pitt.edu>
2023-04-02 15:26:35 -0400

    Add BottomNavTab

---
+++ CommunityCompiler/Views/BottomNavTab.xaml.cs
@@ -0,0 +1,24 @@
+﻿namespace CommunityCompiler.Views;
+
+public partial class HomeView : ContentPage
+{
+	public HomeView()
+	{
+		InitializeComponent();
+	}
+
+    private async void favoriteBtn_clicked(object sender, EventArgs e)
+    {
+        await Navigation.PushAsync(new FavoritesView());
+    }
+
+    private async void searchEventBtn_clicked(object sender, EventArgs e)
+    {
+        await Navigation.PushAsync(new SearchEventsView());
+    }
+
+    private async void profileSettingBtn_clicked(object sender, EventArgs e)
+    {
+        await Navigation.PushAsync(new SettingView());
+    }
+}
