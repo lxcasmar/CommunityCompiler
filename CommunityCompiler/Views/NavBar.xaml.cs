18ae0c3ee6b3a5109f049aff6eabd51acf7f44c1
Vy Nguyen <93799211+vyn203@users.noreply.github.com>
2023-04-07 23:03:13 -0400

    Update NavBar.xaml.cs

--- CommunityCompiler/Views/NavBar.xaml.cs
+++ CommunityCompiler/Views/NavBar.xaml.cs
@@ -1,15 +1,57 @@
-﻿using System.Windows.Input;
-using CommunityCompiler.ViewModels;
-using CommunityCompiler.Services;
+using Microsoft.Maui.Controls;
+using Microsoft.Maui.Controls.Platform;
 
 namespace CommunityCompiler.Views;
 
-public partial class NavBar : ContentView
+public partial class NavBar : TabbedPage
 {
-	public NavBar()
-	{
-		InitializeComponent();
-		BindingContext = ServiceAid.GetService<NavBarViewModel>();
-	}
+    public NavBar()
+    {
+        InitializeComponent();
 
+        nav_home.IconImageSource = "selected_home.png";
+        nav_search.IconImageSource = "unselected_search.png";
+        nav_settings.IconImageSource = "unselected_settings.png";
+        nav_favorites.IconImageSource = "unselected_favorites.png";
+
+        CurrentPageChanged += CurrentPageHasChanged;
+    }
+
+    private void CurrentPageHasChanged(object sender, EventArgs e)
+    {
+        var tabbedPage = (TabbedPage)sender;
+        Title = tabbedPage.CurrentPage.Title;
+
+        if (Title == "Home")
+        {
+            nav_home.IconImageSource = "selected_home.png";
+            nav_search.IconImageSource = "unselected_search.png";
+            nav_settings.IconImageSource = "unselected_settings.png";
+            nav_favorites.IconImageSource = "unselected_favorites.png";
+        }
+
+        if (Title == "Search")
+        {
+            nav_home.IconImageSource = "unselected_home.png";
+            nav_search.IconImageSource = "selected_search.png";
+            nav_settings.IconImageSource = "unselected_settings.png";
+            nav_favorites.IconImageSource = "unselected_favorites.png";
+        }
+
+        if (Title == "Settings")
+        {
+            nav_home.IconImageSource = "unselected_home.png";
+            nav_search.IconImageSource = "unselected_search.png";
+            nav_settings.IconImageSource = "selected_settings.png";
+            nav_favorites.IconImageSource = "unselected_favorites.png";
+        }
+
+        if (Title == "Favorites")
+        {
+            nav_home.IconImageSource = "unselected_home.png";
+            nav_search.IconImageSource = "unselected_search.png";
+            nav_settings.IconImageSource = "unselected_settings.png";
+            nav_favorites.IconImageSource = "selected_favorites.png";
+        }
+    }
 }
