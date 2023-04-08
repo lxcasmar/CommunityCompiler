c67956e14bb671ff3d2cbd11090ed5c189205f07
vyn203 <vyn1@pitt.edu>
2023-04-08 16:05:56 -0400

    Add header and change back navbar

---
+++ CommunityCompiler/Controls/PageHeader.xaml.cs
@@ -0,0 +1,12 @@
+using Microsoft.Maui.Controls;
+
+namespace CommunityCompiler.Controls
+{
+    public partial class PageHeader : ContentView
+    {
+        public PageHeader()
+        {
+            InitializeComponent();
+        }
+    }
+}
